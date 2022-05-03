d2b7dbb165707778ef960d59c4b2c9798fd4723b
Arnold D. Robbins <arnold@skeeve.com>
2022-05-03 22:53:45 +0300

    Deprecate MPFR.

--- main.c
+++ main.c
@@ -66,6 +66,7 @@ static void catchstackoverflow(int emergency, stackoverflow_context_t scp);
 #endif
 static void nostalgia(void) ATTRIBUTE_NORETURN;
 static void version(void) ATTRIBUTE_NORETURN;
+static void deprecate_mpfr(void);
 static void init_fds(void);
 static void init_groupset(void);
 static void save_argv(int, char **);
@@ -1686,6 +1687,7 @@ parse_args(int argc, char **argv)
 #else
 			warning(_("-M ignored: MPFR/GMP support not compiled in"));
 #endif
+			deprecate_mpfr();
 			break;
 
 		case 'P':
@@ -1852,4 +1854,23 @@ set_current_namespace(const char *new_namespace)
 		efree((void *) current_namespace);
 
 	current_namespace = new_namespace;
+}
+
+/* deprecate_mpfr --- print long info about MPFR being deprecated */
+
+static void
+deprecate_mpfr(void)
+{
+	static bool first = true;
+
+	if (first) {
+		first = false;
+		if (getenv("GAWK_NO_MPFR_WARN") != NULL)
+			return;
+
+		warning(_("MPFR mode is deprecated!"));
+		warning(_("It will be removed in the gawk release of 2024."));
+		warning(_("For more information, see https://......."));
+		warning(_("Use `export GAWK_NO_MPFR_WARN=1' to disable this warning."));
+	}
 }
